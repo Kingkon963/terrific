a870b17994076fb56c068d2b2c79c1c97d631a66
Naimul Islam <naimulislam19149@gmail.com>
2024-09-24 13:06:29 +0600

    fixed path

--- src/app/ai/flows/index.ts
+++ src/app/ai/flows/index.ts
@@ -2,6 +2,7 @@ import { googleAI } from '@genkit-ai/googleai';
 import { incidentAnalyzerFlow } from './incidentAnalyzerFlow';
 import { configureGenkit } from '@genkit-ai/core';
 import { dotprompt } from '@genkit-ai/dotprompt';
+import path from 'path';
 
 
 
@@ -11,7 +12,7 @@ configureGenkit({
             apiKey: process.env.GOOGLE_GENAI_API_KEY,
         }),
         dotprompt({
-            dir: "src/app/ai/prompts",
+            dir:  path.join("src", "app", "ai", "prompts"),
         })
     ],
     logLevel: process.env.NODE_ENV === 'development' ? 'debug' : 'info',
